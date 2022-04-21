42602078c313ed1d5d13f8a4f300fca90e0a13ce
AnastasiaJunger <100357987+AnastasiaJunger@users.noreply.github.com>
2022-04-21 17:01:08 +0300

    Add files via upload

---
+++ java/src/com/epam/training/student_anastasiia_iunger/tasks/meet_a_stranger/MeetAStranger.java
@@ -0,0 +1,11 @@
+package com.epam.training.student_anastasiia_iunger.tasks.meet_a_stranger;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println("Hello, " + input);
+    }
+}
---
+++ java/src/com/epam/training/student_anastasiia_iunger/tasks/meet_autocode/HelloAutocode.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_anastasiia_iunger.tasks.meet_autocode;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        System.out.println("Hello, Autocode!");
+    }
+}
